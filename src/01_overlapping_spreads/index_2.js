ab01444976057b26ce4397fad83be8711c6e4226
Xande <amlage.cv@gmail.com>
2021-04-21 12:28:52 -0300

    Save improved version using objects

---
+++ src/01_overlapping_spreads/index_2.js
@@ -0,0 +1,24 @@
+// Given an array of integers, replace every element with its neighbouring numbers (integers next to it),
+// then find the duplicate numbers and sum them up. It is guaranteed that provided integers are unique,
+// which means they will not repeat.
+// 
+// For example for the input: -3,5,8,-1,6,11
+// * given that neighbouring numbers for -3 are -4 and -2
+// * resulting array after replacement with neighbouring numbers would be: -4,-2,4,6,7,9,-2,0,5,7,10,12 
+// * duplicate numbers are -2,7
+// * the result will be 5.
+
+exports.overlappingSpreads = function (data) {
+    const neighbouringObj = { memory: {}, sum: 0 }
+
+    if (Array.isArray(data)) {
+        data.forEach((value) => {
+            const neighbours = [value-1, value+1]
+            neighbouringObj.memory[neighbours[0]] ? neighbouringObj.sum += neighbours[0] : neighbouringObj.memory[neighbours[0]] = true;
+            neighbouringObj.memory[neighbours[1]] ? neighbouringObj.sum += neighbours[1] : neighbouringObj.memory[neighbours[1]] = true;
+        })
+    }
+
+    return neighbouringObj.sum
+}
+
